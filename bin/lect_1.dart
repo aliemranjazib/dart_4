e3458a53575450d46b8927a0444c4bdd31125e94
aliemranjazib <aliemranjazib@gmail.com>
2021-01-27 20:47:59 +0500

    class structure

--- bin/lect_1.dart
+++ bin/lect_1.dart
@@ -1,12 +1,26 @@
 void main() {
-  print('HI');
-  int a = 10;
-  int b = 50;
-  int result = a + b;
-  print('addition of $a and $b is $result');
-  if (result < 50) {
-    print('you are fail');
-  } else {
-    print('you are pass');
+  var fd = FirstDay(); // this code create object of class
+  fd.intmanup(); // this code will call method
+  fd.stringmanp(); // this code will call method
+}
+
+// this is our first class which have and two functions
+class FirstDay {
+  // this is simple addition fucntion
+  void intmanup() {
+    int a = 10;
+    int b = 20;
+    int result = a + b;
+    print('rsult is $result');
+  }
+
+// this is simple string manipulation function
+  void stringmanp() {
+    String info = "Hi I am ali";
+    if (info.isEmpty) {
+      print('info is empty');
+    } else {
+      print('info is not empty');
+    }
   }
 }
