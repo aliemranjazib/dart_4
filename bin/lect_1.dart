2071469e9b10e8b4c672e9838d4af3a067c44f2f
aliemranjazib <aliemranjazib@gmail.com>
2021-01-27 20:37:01 +0500

    if else

--- bin/lect_1.dart
+++ bin/lect_1.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  print('HI');
+  int a = 10;
+  int b = 50;
+  int result = a + b;
+  print('addition of $a and $b is $result');
+  if (result < 50) {
+    print('you are fail');
+  } else {
+    print('you are pass');
+  }
 }
